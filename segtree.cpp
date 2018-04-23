8f2ad46efe7a8a2bb6af0868b199298622b2ae60
1119-2916 <ti11192916@gmail.com>
2018-04-24 03:24:54 +0900

    [add] README

--- segtree.cpp
+++ segtree.cpp
@@ -1,7 +1,7 @@
     struct Monoid {
         using T = int;
         T operator()(const T &a, const T& b) { return min(a, b); }
-        static const T I = INT_MAX;
+        const T I = INT_MAX;
     };
     struct SegmentTree {
         using T = Monoid::T;
@@ -23,8 +23,8 @@
         T query(int a, int b, int k, int l, int r) {
             if (r <= a || b <= l)return op.I;
             if (a <= l&&r <= b)return d[k];
-            int xl = query(a, b, 2 * k + 1, l, (l + r) / 2);
-            int xr = query(a, b, 2 * k + 2, (l + r) / 2, r);
+            T xl = query(a, b, 2 * k + 1, l, (l + r) / 2);
+            T xr = query(a, b, 2 * k + 2, (l + r) / 2, r);
             return op(xl, xr);
         }
         T query(int a, int b) {
